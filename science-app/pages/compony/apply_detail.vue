7df0496f84273d2f68bedd0ff63519dbd6a273c4
yan qi <1343277436@qq.com>
2020-07-28 09:05:27 +0800

    yq2

--- science-app/pages/compony/apply_detail.vue
+++ science-app/pages/compony/apply_detail.vue
@@ -1,6 +1,29 @@
 <template>
-	<view>
-		申请详情
+	<view class="content">
+		<view class="box">
+			<view class="top">
+				<view class="top_title">
+					基础信息
+				</view>
+				<view class="top_p">
+					为贯彻落实省政府《山西省深化省级财政科技计划专项基金等管理改革方案》，规范项目管理专业机构，我们制定了
+					《山西省省级财政科技计划 (专项，基金等)项目管理专业机构管理暂行规范》
+				</view>
+				<view class="top_p">
+					经厅际联席会审定并经法制办审查通过，现正式印发请认真执行。
+					附件：《山西省省级财政科技计划（专项、基金等）项目管理专业机构管理暂行规定》
+				</view>
+				<view class="top_title">
+					联系电话
+				</view>
+				<view class="top_phone">
+					189-3948-0086
+				</view>
+			</view>
+			<view class="bottom">
+				详情查询
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -18,5 +41,54 @@
 </script>
 
 <style>
-
+.content{
+	width:100% ;
+	height:100% ;
+}
+.box{
+	width:100% ;
+	height:1107rpx ;
+	padding-top:30rpx ;
+}
+.top{
+	width:652rpx ;
+	height:709rpx ;
+	padding-top:50rpx ;
+	margin:0rpx auto ;
+	box-shadow: 0px 0px 15px #f6f6f6;
+	border-radius:8rpx ;
+	margin-bottom:0 ;
+}
+.top_title{
+	padding-left:34rpx ;
+	height:76rpx ;
+	font-size:27rpx ;
+	color: #a5a7af;
+}
+.top_p{
+	padding-left:34rpx ;
+	padding-right:45rpx ;
+	font-size:22rpx ;
+	line-height:35rpx ;
+	height:180rpx ;
+	color: #565970;
+	letter-spacing:5rpx ;
+	
+}
+.top_phone{
+	padding-left:34rpx ;
+	font-size:22rpx ;
+	color:#fdab3e ;
+}
+.bottom{
+	width:702rpx ;
+	height:100rpx ;
+	margin:293rpx auto ;
+	background-color:#20c8bb ;
+	text-align: center;
+	line-height:100rpx ;
+	color:#fff;
+	border-radius:8rpx ;
+	margin-bottom:0 ;
+}
 </style>
--- science-app/pages/compony/info.vue
+++ science-app/pages/compony/info.vue
@@ -1,78 +1,93 @@
 <template>
 	<view class="content">
-		<!-- 所有人员开始 -->
-		<view class="all">
-			 <view class="status">
-				<view class="status_public left">
-					<image src="@/static/compont/info_4.png" class="status_img"></image>
-					<view class="status_text1">所有人员</view>
-					<view class="status_text2"><span>268</span>人</view>
-				</view>
-				<view class="status_public right">
-					<image src="@/static/compont/info_3.png" class="status_img"></image>
-					<view class="status_text1">研发人员</view>
-					<view class="status_text2"><span>64</span>人</view>
-				</view>
-			</view> 
-			
-			 <view class="personnel">
-				<view class="personnel_list">
-					<image src="@/static/compont/info_1.png"  class="personnel_img"></image>
-					<view class="personnel_text1">苏婵</view>
-					<view class="personnel_text2">研究生</view>
-					<view class="personnel_text3">职称：高级技术员</view>
-				</view>
-				<view class="personnel_list">
-					<image src="@/static/compont/info_1.png"  class="personnel_img"></image>
-					<view class="personnel_text1">苏婵</view>
-					<view class="personnel_text2">研究生</view>
-					<view class="personnel_text3">职称：高级技术员</view>
-				</view>
-				<view class="personnel_list">
-					<image src="@/static/compont/info_1.png"  class="personnel_img"></image>
-					<view class="personnel_text1">苏婵</view>
-					<view class="personnel_text2">研究生</view>
-					<view class="personnel_text3">职称：高级技术员</view>
+		<view class="top">
+			<view :class="['top_1',{active:state===1}]" @click="state=1">
+				人员状况
+			</view>
+			<view :class="['top_1',{active:state===2}]" @click="state=2">
+				营业执照
+			</view>
+			<view :class="['top_1',{active:state===3}]" @click="state=3">
+				自由技术结果
+			</view>
+		</view>
+		
+		<view class="bottom">
+			<view :class="['all',{actives:state ===1}]">
+				 <view class="status">
+					<view class="status_public left">
+						<image src="@/static/compont/info_4.png" class="status_img"></image>
+						<view class="status_text1">所有人员</view>
+						<view class="status_text2"><span>268</span>人</view>
+					</view>
+					<view class="status_public right">
+						<image src="@/static/compont/info_3.png" class="status_img"></image>
+						<view class="status_text1">研发人员</view>
+						<view class="status_text2"><span>64</span>人</view>
+					</view>
 				</view> 
-			</view>
-		</view>
-		<!-- 所有人员结束 -->
-		
-		
-		<!-- 营业执照开始 -->
-		<view class="license">
-			<view class="business">
-				<image src="@/static/compont/info_5.png" mode=""></image>
-			</view>
-		</view>
-		<!-- 营业执照结束 -->
-		
-		
-		<!--自有技术成果开始  -->
-		<view class="start">
-			<view class="results">
-				<view class="results_list">
-					<image src="@/static/compont/info_2.png" class="results_img1"></image>
-					<view class="results_text1">自由研究成果一</view>
-					<image src="@/static/compont/info_6.png" class="results_img2"></image>
-					<view class="results_text2">研究成果的具体文字描述，最多20个字符</view>
-				</view>
-				<view class="results_list">
-					<image src="@/static/compont/info_2.png" class="results_img1"></image>
-					<view class="results_text1">自由研究成果一</view>
-					<image src="@/static/compont/info_6.png" class="results_img2"></image>
-					<view class="results_text2">研究成果的具体文字描述，最多20个字符</view>
-				</view>
-				<view class="results_list">
-					<image src="@/static/compont/info_2.png" class="results_img1"></image>
-					<view class="results_text1">自由研究成果一</view>
-					<image src="@/static/compont/info_6.png" class="results_img2"></image>
-					<view class="results_text2">研究成果的具体文字描述，最多20个字符</view>
+				
+				 <view class="personnel">
+					<view class="personnel_list">
+						<image src="@/static/compont/info_1.png"  class="personnel_img"></image>
+						<view class="personnel_text1">苏婵</view>
+						<view class="personnel_text2">研究生</view>
+						<view class="personnel_text3">职称：高级技术员</view>
+					</view>
+					<view class="personnel_list">
+						<image src="@/static/compont/info_1.png"  class="personnel_img"></image>
+						<view class="personnel_text1">苏婵</view>
+						<view class="personnel_text2">研究生</view>
+						<view class="personnel_text3">职称：高级技术员</view>
+					</view>
+					<view class="personnel_list">
+						<image src="@/static/compont/info_1.png"  class="personnel_img"></image>
+						<view class="personnel_text1">苏婵</view>
+						<view class="personnel_text2">研究生</view>
+						<view class="personnel_text3">职称：高级技术员</view>
+					</view> 
 				</view>
 			</view>
+			<!-- 所有人员结束 -->
 			
+			
+			<!-- 营业执照开始 -->
+			<view :class="['license',{actives:state ===2}]">
+				<view class="business">
+					<image src="@/static/compont/info_5.png" mode=""></image>
+				</view>
+			</view>
+			<!-- 营业执照结束 -->
+			
+			
+			<!--自有技术成果开始  -->
+			<view :class="['start',{actives:state ===3}]">
+				<view class="results">
+					<view class="results_list">
+						<image src="@/static/compont/info_2.png" class="results_img1"></image>
+						<view class="results_text1">自由研究成果一</view>
+						<image src="@/static/compont/info_6.png" class="results_img2"></image>
+						<view class="results_text2">研究成果的具体文字描述，最多20个字符</view>
+					</view>
+					<view class="results_list">
+						<image src="@/static/compont/info_2.png" class="results_img1"></image>
+						<view class="results_text1">自由研究成果一</view>
+						<image src="@/static/compont/info_6.png" class="results_img2"></image>
+						<view class="results_text2">研究成果的具体文字描述，最多20个字符</view>
+					</view>
+					<view class="results_list">
+						<image src="@/static/compont/info_2.png" class="results_img1"></image>
+						<view class="results_text1">自由研究成果一</view>
+						<image src="@/static/compont/info_6.png" class="results_img2"></image>
+						<view class="results_text2">研究成果的具体文字描述，最多20个字符</view>
+					</view>
+				</view>
+				
+			</view>
+			<!-- 自由技术成果结束 -->
 		</view>
-		<!-- 自由技术成果结束 -->
+		<!-- 所有人员开始 -->
+		
 	</view>
 </template>
 
@@ -80,7 +95,7 @@
 	export default {
 		data() {
 			return {
-				
+				 state:1
 			}
 		},
 		methods: {
@@ -94,11 +109,42 @@
 		width:100% ;
 		height:100% ;
 	}
+	.top{
+		width:725rpx ;
+		height:45rpx ;
+		padding-left:25rpx ;
+		padding-top:23rpx ;
+		overflow: hidden;
+		background-color:#2c304c ;
+		font-size:23rpx ;
+		
+	}
+	.top_1{
+		color:#7e808f;
+		float: left;
+		margin-right:64rpx ;
+	}
+	.active{
+		color:#ffffff;
+		position: relative;
+	}
+	.active::after{
+		content: '';
+		width:34rpx ;
+		height:4rpx ;
+		position: absolute;
+		top:40rpx;
+		left:34%;
+		background-color:#fdab3e ;	
+	}
+	.bottom{
+		width:100% ;
+		height:1130rpx ;
+	}
 	/* 所有人开始 */
 	.all{
 		width:100% ;
-		height:auto ;
-		overflow: hidden;
+		height:1130rpx ;
 		display:none ;
 	}
 	.status{
@@ -199,8 +245,7 @@
 	/* 营业执照开始 */
 	.license{
 		width:100% ;
-		height:auto;
-		overflow: hidden;
+		height:1130rpx;
 		display: none;
 	}
 	.business{
@@ -217,8 +262,8 @@
 	/* 自有技术成果开始 */
 	.start{
 		width:100% ;
-		height:auto;
-		overflow: hidden;
+		height:1130rpx;
+		display:none ;
 	}
 	.results{
 		width:660rpx ;
@@ -262,4 +307,7 @@
 		margin-top:54rpx ;
 	}
 	/* 自有技术成果结束 */
+	.actives{
+		display: block;
+	}
 </style>
